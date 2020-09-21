bfd9775fffb14a55ae553e8d5c2167e688b6d420
thalsi <thalseehthalsi609@gmail.com>
2020-09-21 14:07:05 +0530

    this bacestypr

--- fast.js
+++ fast.js
@@ -1,70 +1,38 @@
-var num = 34;
-var str = "hello";
-var bool = false;
-var voide = undefined;
-var numbe = null;
-var list = [1, 2, 3];
-var mulList = [1, 3, "koi", 4];
-var toul = ["koi", 1];
-var emplly = [[1, "ammu"], [2, "lop"], [3, "tha"]];
+var a = true;
+var b = false;
+var n = 45;
+var s = "hello world";
+var se = 'hello world';
+console.log(a);
+console.log(b);
+console.log(n);
+console.log(s);
+console.log(se);
+console.log("got are new " + ("" + se));
+console.log("hi" + "\n" + "age");
+var list = [1, 2, 3, 4];
+console.log(list + "\n" + list[0]);
+var tupe = [1, "ali"];
+var tupeList = [1, "ammu", 2, "annu"];
+console.log("tuple--->" + tupe);
+console.log(tupe[0]);
+console.log(tupe[1]);
+console.log("tupleList---->" + tupeList);
 var color;
 (function (color) {
-    color[color["red"] = 3] = "red";
-    color[color["green"] = 4] = "green";
-    color[color["blue"] = 5] = "blue";
+    color[color["bule"] = 0] = "bule";
+    color[color["green"] = 1] = "green";
+    color[color["red"] = 2] = "red";
 })(color || (color = {}));
-;
-var union_1;
-console.log(num);
-console.log(str);
-console.log(bool);
-console.log(voide);
-console.log(numbe);
-voide = null;
-console.log(voide);
-console.log("numrical array-->" + list);
-console.log("multple data type array-->" + mulList);
-console.log(toul);
-console.log("employ are tuple--->" + emplly);
-console.log("print enum---->" + color.red);
-union_1 = "thalsi";
-console.log("print union----->" + union_1);
-union_1 = 123;
-console.log("print union------>" + union_1);
-function hello() {
-    console.log('koi');
-    return "hello world";
-}
-console.log(hello());
-function voidFun() {
-    console.log("voide Fuction");
-}
-console.log(voidFun());
-var a = null;
-var b = undefined;
-if (a == b) {
-    console.log("undefind and null are equal-->" + a + "=" + b);
-}
-var anyV;
-anyV = "string";
-console.log(anyV);
-anyV = 213;
-console.log(anyV);
-function anyFunction(a, b) {
-    return a + b;
-}
-console.log(anyFunction("hi", "annu"));
-console.log(anyFunction(1, 3));
-var k1 = { key: 1, value: "String" };
-console.log(k1);
-var Empl = /** @class */ (function () {
-    function Empl(name) {
-        this.name = name;
-    }
-    Empl.prototype.dispaly = function () {
-        console.log(this.name);
-    };
-    return Empl;
-}());
-var ee = new Empl("koi");
-ee.dispaly();
+console.log(color);
+console.log(color[2]);
+console.log(color.green);
+var e = color.green;
+console.log("--------" + e);
+var un = "say,hi";
+console.log(un);
+un = 35;
+console.log(un);
+var ab = 676;
+ab = "fdg";
+console.log(ab);
--- fast.ts
+++ fast.ts
@@ -1,84 +1,42 @@
-let num:number =34;
-let str:string ="hello";
-let bool:boolean= false;
-let voide:void=undefined;
-let numbe:number=null;
-let list:Array<number>=[1, 2, 3];
-let mulList:Array<string | number>=[1,3,"koi",4];
-let toul: [string,number] =["koi",1];
-let emplly:[number, string][] =[[1,"ammu"],[2,"lop"],[3,"tha"]];
+var a:boolean=true;
+var b:boolean=false;
+var n:number=45;
+var s:string="hello world";
+var se:string='hello world';
+console.log(a);
+console.log(b);
+console.log(n);
+console.log(s);
+console.log(se);
+console.log("got are new "+`${se}`);
+console.log("hi"+"\n"+"age");
+
+var list:number[]=[1, 2, 3, 4];
+console.log(list+"\n"+list[0]);
+
+var tupe:[number,string]=[1, "ali"];
+var tupeList:[number,string, number, string]=[1, "ammu", 2, "annu"];
+console.log("tuple--->"+tupe);
+console.log(tupe[0]);
+console.log(tupe[1]);
+
+console.log("tupleList---->"+tupeList);
+
 enum color{
-    red=3,green,blue
-};
-let union_1:(string | number);
-
-console.log(num);
-console.log(str);
-console.log(bool);
-console.log(voide);
-console.log(numbe);
-voide =null;
-console.log(voide);
-console.log("numrical array-->"+list);
-console.log("multple data type array-->"+mulList);
-console.log(toul);
-console.log("employ are tuple--->"+emplly);
-console.log("print enum---->"+color.red);
-union_1="thalsi";
-console.log("print union----->"+union_1);
-union_1=123;
-console.log("print union------>"+union_1);
-
-function hello(){
-    console.log('koi');
-    return "hello world";
+    bule,
+    green,
+    red
 }
-console.log(hello());
+console.log(color);
+console.log(color[2]);
+console.log(color.green);
+let e:color=color.green;
+console.log("--------"+e);
+var un:unknown="say,hi";
+console.log(un)
+un=35;
+console.log(un);
 
-function voidFun():void{
-    console.log("voide Fuction");
-}
-console.log(voidFun());
-let a=null;
-let b=undefined;
-if(a==b){
-    console.log("undefind and null are equal-->"+a+"="+b);
-}
-
-let anyV:any;
-anyV="string";
-console.log(anyV);
-anyV=213;
-console.log(anyV);
-
-function anyFunction(a:any, b:any){
-    return a+b;
-}
-
-console.log(anyFunction("hi","annu"));
-console.log(anyFunction(1, 3));
-
-interface keyPair{
-    key:number;
-    value:string;
-}
-
-let k1:keyPair={ key:1, value:"String"};
-console.log(k1);
-
-interface Person{
-    name:string;
-    dispaly():void;
-}
-
-class Empl implements Person{
-    name:string;
-    constructor(name:string){
-        this.name=name;
-    }
-    dispaly(){
-        console.log(this.name);
-    }
-}
-let ee:Person = new Empl("koi");
-ee.dispaly();
+let ab:any=676;
+ab="fdg";
+console.log(ab);
